a613bc03c20aeedf261d85f4f37f27e6470415c5
Mark Matthews <mark.matthews@gmail.com>
2013-09-22 15:05:33 -0400

    Adding files

---
+++ redact.js
@@ -0,0 +1,8 @@
+;(function(){
+document.body.style.background = red;
+document.body.style.backgroundColor= '#990000';
+//document.getElementsByTagName('body')[0].style.setProperty('font-family','serif', 'important')
+});
+
+
+
