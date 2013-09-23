3fd68e1f4b3f78994c9e915df622689177e13961
Mark Matthews <mark.matthews@gmail.com>
2013-09-22 23:17:27 -0400

    new version

--- redact.js
+++ redact.js
@@ -1,26 +1,6 @@
 (function(){
-alert('yo bitches');
 
-document.getElementsByTagName('body')[0].style.setProperty('font-family','serif', 'important');
-
-
-$(document).click(function (){
-
-
-    var selectedText = window.getSelection ? window.getSelection() : document.selection.createRange(); // second one for IE
-
-
-    if (selectedText.getRangeAt) {
-        var range = selectedText.getRangeAt(0);
-        var newNode = document.createElement("span");
-        newNode.setAttribute('class', 'highlightedText');
-        range.surroundContents(newNode);
-    } else {
-        selectedText.pasteHTML('<span class="highlightedText">' + selectedText.htmlText + '</span>');
-
-    }
-    $('.highlightedText').replaceWith(swapText);
-});
+//document.getElementsByTagName('body')[0].style.setProperty('font-family','serif', 'important');
 
 
 function addCSSRule(sheet, selector, rules, index) {
@@ -34,6 +14,25 @@ function addCSSRule(sheet, selector, rules, index) {
 
 
 addCSSRule(document.styleSheets[0], ".highlightedText",  "background-color : black", 0 );
+
+
+
+$(document).click(function (){
+
+ var selectedText = window.getSelection ? window.getSelection() : document.selection.createRange(); // second one for IE
+
+if (selectedText.getRangeAt) {
+        var range = selectedText.getRangeAt(0);
+        var newNode = document.createElement("span");
+        newNode.setAttribute('class', 'highlightedText');
+        range.surroundContents(newNode);
+    } else {
+        selectedText.pasteHTML('<span class="highlightedText">' + selectedText.htmlText + '</span>');
+
+    }
+    $('.highlightedText').replaceWith(swapText);
+});
+
 
 
 
