9a8d17f04c4a21b1f0bb208f77c1e94db013bf55
Mark Matthews <mark.matthews@gmail.com>
2013-09-22 23:07:55 -0400

    new change

--- redact.js
+++ redact.js
@@ -1,16 +1,15 @@
 (function(){
 alert('yo bitches');
-//document.body.style.background = red;
-//document.body.style.backgroundColor= '#990000';
+
 document.getElementsByTagName('body')[0].style.setProperty('font-family','serif', 'important');
 
 
 $(document).click(function (){
- //alert('yo bitches');
+
+
     var selectedText = window.getSelection ? window.getSelection() : document.selection.createRange(); // second one for IE
 
-   /// var textd = $("#data").html();
-    ///var normalText = $("#data").text();
+
     if (selectedText.getRangeAt) {
         var range = selectedText.getRangeAt(0);
         var newNode = document.createElement("span");
@@ -33,8 +32,8 @@ function addCSSRule(sheet, selector, rules, index) {
 	}
 }
 
-// Use it!
-addCSSRule(document.styleSheets[0], ".highlightedText",  "background-color : black" );
+
+addCSSRule(document.styleSheets[0], ".highlightedText",  "background-color : black", 0 );
 
 
 
